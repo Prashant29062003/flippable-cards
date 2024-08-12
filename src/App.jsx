f007fd02b617d798a658a74df745c09479fe4915
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2024-08-12 03:41:56 +0000

    make pulls

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
+<<<<<<< HEAD
 import {Carousel} from "./components/Carousel/Carousel"
 
 
+=======
+>>>>>>> 850670c (make pulls)
 import './App.css'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
 
   return (
     <>
+<<<<<<< HEAD
       <Carousel/>
+=======
+      <Header/>
+      <Main/>
+      <Footer/>
+>>>>>>> 850670c (make pulls)
     </>
   )
 }
