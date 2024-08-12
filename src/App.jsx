ae846f1b0727ecfb2eb50c0ae394ffa3321692e2
Prashant29062003 <112088696+Prashant29062003@users.noreply.github.com>
2024-08-12 03:49:06 +0000

    make app.jsx error free

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-import {Carousel} from "./components/Carousel/Carousel"
-
-
-=======
->>>>>>> 850670c (make pulls)
 import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
@@ -15,13 +9,9 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
-      <Carousel/>
-=======
       <Header/>
       <Main/>
       <Footer/>
->>>>>>> 850670c (make pulls)
     </>
   )
 }
